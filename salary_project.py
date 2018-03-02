5a87651bd658ec579c7ef56b188465bbce367e57
Ariel Choum <31148540+arieldeveloper@users.noreply.github.com>
2018-03-02 08:57:49 -0500

    Added python script
    A machine learning algorithm that used Simple Linear Regression. Used Numpy, Matplotlib, Pandas.

---
+++ salary_project.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 10:46:15 2018
+
+@author: arielchouminov
+"""
+
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('/Users/arielchouminov/Desktop/Machine-learning/Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_linear_Regression/Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+#Linear regression model being applied to the sets of data we have 
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+
+regressor.fit(X_train, y_train)
+
+#Predicting the test set results 
+y_pred = regressor.predict(X_test)
+
+#Visualizing the training set results 
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train,regressor.predict(X_train), color = 'blue')
+plt.title('Salary based on Experience (training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualizing the Test set results 
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train,regressor.predict(X_train), color = 'blue')
+plt.title('Salary based on Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
